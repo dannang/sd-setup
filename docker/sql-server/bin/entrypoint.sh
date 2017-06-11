6dbaf04aa5c821214d3225f76651a6254d330b20
DaNna Negrescu <ng.danna@gmail.com>
2017-06-12 01:58:25 +0300

    Added data import to docker

--- docker/sql-server/bin/entrypoint.sh
+++ docker/sql-server/bin/entrypoint.sh
@@ -3,13 +3,17 @@
 /opt/mssql/bin/sqlservr &
 
 # Wait for database availability
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -Q "select 1"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -S localhost -U sa -P "${SA_PASSWORD}" -Q "select 1"
 
 echo "Database configuration in progress..."
 
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -Q "create database ${SQLSERVER_DATABASE};"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -Q "create login ${SQLSERVER_USER} with password = '${SQLSERVER_PASSWORD}', DEFAULT_DATABASE=[${SQLSERVER_DATABASE}];"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -d "${SQLSERVER_DATABASE}" -Q "create user ${SQLSERVER_USER}"
-/opt/mssql-tools/bin/sqlcmd -S localhost -U SA -S localhost -U SA -P "${SA_PASSWORD}" -d "${SQLSERVER_DATABASE}"  -Q "EXEC sp_addrolemember N'db_owner', N'${SQLSERVER_USER}';"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -S localhost -U sa -P "${SA_PASSWORD}" -Q "create database ${SQLSERVER_DATABASE};"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -S localhost -U sa -P "${SA_PASSWORD}" -Q "create login ${SQLSERVER_USER} with password = '${SQLSERVER_PASSWORD}', DEFAULT_DATABASE=[${SQLSERVER_DATABASE}];"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -S localhost -U sa -P "${SA_PASSWORD}" -d "${SQLSERVER_DATABASE}" -Q "create user ${SQLSERVER_USER}"
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa -S localhost -U sa -P "${SA_PASSWORD}" -d "${SQLSERVER_DATABASE}"  -Q "EXEC sp_addrolemember N'db_owner', N'${SQLSERVER_USER}';"
 
 echo "Database started"
+
+wait
+
+/usr/local/bin/import-data.sh
---
+++ docker/sql-server/bin/import-data.sh
@@ -0,0 +1,11 @@
+#!bin/bash
+
+#wait for the SQL Server to come up
+sleep 90s
+
+#run the setup script to create the DB and the schema in the DB
+/opt/mssql-tools/bin/sqlcmd -S localhost -U sa  -S localhost -U sa -P "${SA_PASSWORD}" -i /usr/src/data/create-table.sql
+
+#import the data from the given file
+/opt/mssql-tools/bin/bcp historicaldata.dbo.date in "/usr/src/data/Date.csv" -c -t',' -S localhost -U sa -P "${SA_PASSWORD}"
+/opt/mssql-tools/bin/bcp historicaldata.dbo.exchange in "/usr/src/data/Exchange.csv" -c -t',' -S localhost -U sa -P "${SA_PASSWORD}"
